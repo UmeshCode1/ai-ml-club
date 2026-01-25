e63879513d1a764f91dec7ea7fac42dfc78bcd91
Umesh Code <umesh.code1@gmail.com>
2026-01-25 06:32:42 +0530

    refactor(pwa): remove install popup overlay per user request

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ import { RouteTransition } from "@/components/layout/route-transition";
 import { PreLoader } from "@/components/layout/pre-loader";
 import { GlobalBackground } from "@/components/layout/global-background";
 import { ScrollToTop } from "@/components/layout/scroll-to-top";
-import { InstallPWA } from "@/components/layout/install-pwa";
+
 
 const inter = Inter({
   subsets: ["latin"],
@@ -116,7 +116,7 @@ export default function RootLayout({
             <div className="fixed inset-0 z-[9999] pointer-events-none opacity-[0.03] mix-blend-overlay bg-[url('/noise.png')] bg-repeat" />
 
             <AppwritePing />
-            <InstallPWA />
+            {/* <InstallPWA /> Removed per user request */}
             <ScrollToTop />
             <PreLoader />
             <GlobalBackground />
